898a490c724e77e69f349fcf89f8ed9323581967
Rafo Bitro <67033048+rafobitro@users.noreply.github.com>
2026-03-06 20:35:43 -0800

    ubdate config list to find steam

--- instal_script.sh
+++ instal_script.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
+set -e
 
+echo "Updating system"
+sudo sed -i '/\[multilib\]/,/Include/ s/^#//' /etc/pacman.conf
 sudo pacman -Syu --noconfirm
 
 echo "Installing pacman packages"
 sudo pacman -S --needed --noconfirm - < packages.txt
+
+echo "Installing Flatpak"
+sudo pacman -S --needed --noconfirm flatpak
+
+echo "Adding Flathub repository"
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo "Installing flatpak apps"
 while read app; do
