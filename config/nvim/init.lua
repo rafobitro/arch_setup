b6b0d5610921dad4df66b65ff6aeacdbea07daa0
Rafo Bitro <67033048+rafobitro@users.noreply.github.com>
2026-03-07 12:38:15 -0800

    fix some error meybe

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -999,3 +999,31 @@ require('lazy').setup({
 	-- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
 	ui = {
+		-- If you are using a Nerd Font: set icons to an empty table which will use the
+		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = '⌘',
+			config = '🛠',
+			event = '📅',
+			ft = '📂',
+			init = '⚙',
+			keys = '🗝',
+			plugin = '🔌',
+			runtime = '💻',
+			require = '🌙',
+			source = '📄',
+			start = '🚀',
+			task = '📌',
+			lazy = '💤 ',
+		},
+	},
+})
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
+-- Force OLED black background
+vim.opt.background = "dark"
+vim.cmd("highlight Normal guibg=#000000 ctermbg=0")
+vim.cmd("highlight NormalNC guibg=#000000 ctermbg=0")
+vim.cmd("highlight SignColumn guibg=#000000 ctermbg=0")
+vim.cmd("highlight LineNr guibg=#-000001 ctermbg=0")
